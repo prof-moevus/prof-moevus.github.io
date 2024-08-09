afae33efa829d06d704787c75d44390cfa9ef10f
prof-moevus <106986122+prof-moevus@users.noreply.github.com>
2024-08-09 13:07:48 -0400

    :tada: Create new portfolio with Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Prof-moevus rules!</h1>
+      <p>Learn how to code with me</p>
+    </main>
+  );
+}
