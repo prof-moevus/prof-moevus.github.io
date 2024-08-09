f0fad3315aa12ea49948c30acb6e82dc427d9523
prof-moevus <106986122+prof-moevus@users.noreply.github.com>
2024-08-09 13:16:26 -0400

    :lipstick: Improve site appearance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1>Prof-moevus rules!</h1>
       <p>Learn how to code with me</p>
     </main>
---
+++ src/app/projets/page.tsx
@@ -0,0 +1,8 @@
+export default function Projets() {
+    return (
+        <>
+        <h1>Projets</h1>
+            <p className={"underline"}>Voici mes projets :</p>
+        </>
+    )
+}
